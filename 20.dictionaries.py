dde917e90c0135e072f815d9e4a3870d476adf4f
Aliia <aisakhodzhieva@gmail.com>
2021-10-29 21:59:36 +0600

    28.10.

---
+++ 20.dictionaries.py
@@ -0,0 +1,19 @@
+#colors = {'c1': 'Red', 'c2': 'Grey', 'c3': None, 'c4': 'Green', 'c5': 'Yellow', 'c6': 'Pink',
+#          'c7': 'Orange', 'c8': None, 'c9': 'White', 'c10': 'Black', 'c11': 'Violet',
+#          'c12': 'Gold', 'c13': None, 'c14': 'Amber', 'c15': 'Azure', 'c16': 'Beige',
+#         'c17': 'Bronze', 'c18': None, 'c19': 'Lilac', 'c20': 'Pearl', 'c21': None,
+#         'c22': 'Sand', 'c23': None}
+
+#result = {i: colors[i] for i in colors if colors[i] != None}
+
+#print(result)
+
+#months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+
+#result = {key: value for value, key in months.items()}
+#print(result)
+
+s = '1:men 2:kind 90:number 0:sun 34:book 56:mountain 87:wood 54:car 3:island 88:power 7:box 17:star 101:ice'.split(',:')
+#result = {int(i[0]): i[1] for i in i.split() in s.split()}
+
+print(s)
