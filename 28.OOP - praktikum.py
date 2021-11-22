48affe7b0164cfdba2825d9cd041c4e8b348ba25
Aliia <aisakhodzhieva@gmail.com>
2021-11-22 22:48:14 +0600

    22.11

---
+++ 28.OOP - praktikum.py
@@ -0,0 +1,47 @@
+import math
+# class Contact:
+#     def __init__(self, name, phone, address, birthday):
+#         self.name = name
+#         self.phone = phone
+#         self.address = address
+#         self.birthday = birthday
+#         print(f"Создаём новый контакт {name}")
+#
+#
+#
+#     def show_contact(self):
+#         print(f"{self.name} — адрес: {self.address}, телефон: {self.phone}, "
+#               f"день рождения: {self.birthday}")
+#
+#
+# mike = Contact('Михаил Булгаков','2-03-27','Россия, Москва, Большая Пироговская, дом 35б, кв. 6',
+#                '15.05.1891')
+# vlad = Contact('Владимир Маяковский','73-88','Россия, Москва, Лубянский проезд, д. 3, кв. 12','19.07.1893')
+#
+# # def print_contact():
+# #     print(f"{vlad.name} — адрес: {vlad.address}, телефон: {vlad.phone}, "
+# #           f"день рождения: {vlad.birthday}")
+#
+# mike.address = 'Россия, Москва, Нащокинский переулок, дом 3, кв. 44'
+# mike.phone = 'К-058-67'
+# vlad.address = 'Россия, Москва, Гендриков переулок, дом 15, кв. 5'
+# vlad.phone = '2-35-71'
+# mike.show_contact()
+# vlad.show_contact()
+
+
+
+# class Planet:
+#     def __init__(self, name, radius, temp_celsius):
+#         self.name = name
+#         self.surface_area =  4 * math.pi * radius ** 2
+#         self.average_temp_celcius = temp_celsius
+#         self.average_temp_fahrenheit =  temp_celsius * 9/5 + 32
+#
+#     def show_info(self):
+#         print(f"Планета {self.name} имеет площадь поверхности {self.surface_area} кв.км.")
+#         print(f"Средняя температура поверхности планеты: {self.average_temp_fahrenheit}° по Фаренгейту.")
+#
+# jupiter = Planet('Юпитер', 69911, -108)
+#
+# jupiter.show_info()
